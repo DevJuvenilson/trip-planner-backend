62c49747f6f58b577888735adc5fd1e84f1fea40
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-07-10 15:40:21 -0300

    Aula02

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import fastify from "fastify";
 import cors from '@fastify/cors';
-import { createTrip } from "./routes/create-trip";
+import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmParticipants } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/crate-activity";
+import { createLink } from "./routes/create-link";
+import { createTrip } from "./routes/create-trip";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -15,6 +20,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipants);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
